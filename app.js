0c51617b44c1c4a5fc9c508af612b4ad3ca8b2bc
xiongfeng <xiongfeng@qiaobutang.com>
2014-12-31 15:53:04 +0800

    change engine

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
