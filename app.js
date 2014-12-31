d17a86623f17b26ebaa9d8bc791363480cdff936
xiongfeng <xiongfeng@qiaobutang.com>
2014-12-31 16:00:47 +0800

    change routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,8 +23,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
