12513202a66a01dcc75620a650a33a26f5de7205
xiongfeng <xiongfeng165@gmail.com>
2015-01-05 10:55:09 +0800

    add config

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = { 
+    cookieSecret: 'myblog', 
+    db: 'blog', 
+    host: 'localhost',
+    port: 27017,
+    dbPath: 'localhost/blog',
+
+    categories: [
+        ['technology', '技术'],
+        ['util', '工具'],
+        ['source', '资源']
+    ],
+
+    list_topic_count: 10,
+
+    author: {
+        name: 'xiongfeng'
+    },
+
+    title: '博客',
+    description: '博客'
+};
