5b15c3e17a5e73881cfd5692422a262646e79b92
xiongfeng <xiongfeng165@gmail.com>
2015-02-09 22:40:33 +0800

    topic

--- routes/topic.js
+++ routes/topic.js
@@ -340,25 +340,29 @@ exports.getNavs = function(req, res) {
     });
     Topic.groupByTag(function(err, results) {
         var tags = [];
-        results.forEach(function(result) {
-            var record = {};
-            record.name = result._id;
-            record.count = result.count;
-            record.topics = result.topics;
-            tags.push(record);
-        })
+        if (results) {
+            results.forEach(function(result) {
+                var record = {};
+                record.name = result._id;
+                record.count = result.count;
+                record.topics = result.topics;
+                tags.push(record);
+            })
+        }
         ep.emit("tags", tags);
     });
 
     Topic.groupByCategory(function(err, results) {
         var categories = [];
-        results.forEach(function(result) {
-            var record = {};
-            record.name = result._id;
-            record.count = result.count;
-            record.topics = result.topics;
-            categories.push(record);
-        })
+        if (results) {
+            results.forEach(function(result) {
+                var record = {};
+                record.name = result._id;
+                record.count = result.count;
+                record.topics = result.topics;
+                categories.push(record);
+            })
+        }
         ep.emit("categories", categories);
     });
 }
